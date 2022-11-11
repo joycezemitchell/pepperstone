70f3e7e93d05132a7361502d9125b0d2bedc2cab
joycezemitchell <wsi_john@yahoo.com.ph>
2022-11-11 15:27:08 +0800

    Update ctt.go

--- csvprocess/ctt.go
+++ csvprocess/ctt.go
@@ -41,6 +41,7 @@ func ProcesCTT(file string, ch chan bool) error {
 			status = "failed"
 		}
 
+
 		if !errorTransfer {
 			services.AccountService.AddAmount(row, accountTo)    // Update balance of the reciever
 			services.AccountService.DeductAmount(row, accountFrom) // Update balance of the sender
